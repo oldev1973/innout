db209863cb4446c7264b87309b59e2ca84f7166d
Fernando Trindade <trindade-fernando@outlook.com>
2020-12-28 18:09:20 -0300

    classe Database #1

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+$sql = "SELECT * FROM users";
+$result = Database::getResultFromQuery($sql);
+
+
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
+    echo '<br>';
+}
