392bcbe89688738dd3d4229f43adcd622aebb11b
Fernando Trindade <trindade-fernando@outlook.com>
2020-12-29 17:13:49 -0300

    Tela de autenticação

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-// $user = new User(['name' => 'John', 'email' => 'j@mail.com']);
-
-// print_r(User::get(['id' => 1], 'name, email'));
-// echo '<br>';
-
-foreach (User::get([], 'name') as $user) {
-    echo $user->name . '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
