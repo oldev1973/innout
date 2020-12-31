ee2db0c31e3526f5acb4c4434e72d0461989c9bf
Fernando Trindade <trindade-fernando@outlook.com>
2020-12-31 18:04:14 -0300

    Redirecionamento

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -8,7 +8,7 @@ if (count($_POST) > 0) {
     $login = new Login($_POST);
     try {
         $user = $login->checkLogin();
-        echo "Usuário {$user->name} logado :)";
+        header("Location: day_records.php");
     } catch (AppException $e) {
         $exception = $e;
     }
---
+++ src/views/day_records.php
@@ -0,0 +1,1 @@
+<h1>OK day records</h1>
