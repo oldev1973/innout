e4ed1241a40baf93cd480063d537bbfc3b8b4d42
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-01 13:24:35 -0300

    Escondendo menu lateral

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -3,7 +3,7 @@
     <span><i class="icofont-heart text-danger mx1"></i></span>
     <span>por COD<span class="text-danger">3</span>R</span>
 </footer>
-
+<script src="assets/js/app.js"></script>
 </body>
 
 </html>
