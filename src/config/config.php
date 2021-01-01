02842ef4e2cf606d307059d37b4b4a3149c789b6
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-01 17:02:31 -0300

    Gerando massa de dados

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,10 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt-BR', 'pt_BR.utf-8', 'portuguese');
 
+//constantes gerais
+define('DAILY_TIME', 60 * 60 * 8);
+
+
 //Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
