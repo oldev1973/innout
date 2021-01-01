ae6fb726015ee520e5f79af1c86e4987cd0f0a6f
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-01 13:32:21 -0300

    css footer

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
-    <span>Desenvolvido com</span>
-    <span><i class="icofont-heart text-danger mx1"></i></span>
-    <span>por COD<span class="text-danger">3</span>R</span>
+    <span>Desenvolvido com </span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span class="mr-4"> por COD<span class="text-danger">3</span>R</span>
 </footer>
 <script src="assets/js/app.js"></script>
 </body>
