f6edc78de3fc31810bb3db0c8fe04bad1220a969
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-01 14:19:01 -0300

    Tela de registro de ponto

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,4 +1,33 @@
 <main class="content">
-    <h1>OK</h1>
+
+    <div class="content-title mb-4">
+        <i class="icon icofont-check-alt mr-2"></i>
+        <div>
+            <h1>Registrar Ponto</h1>
+            <h2>Mantenha o seu ponto consistente!</h2>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>10 de Janeiro de 2025</h3>
+            <p class="mb-0">Os registros efetuados hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex justify-content-around m-5">
+                <span>Entrada 1: ---</span>
+                <span>Saída 1: ---</span>
+            </div>
+            <div class="d-flex justify-content-around m-5">
+                <span>Entrada 2: ---</span>
+                <span>Saída 2: ---</span>
+            </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+            <a href="???" class="btn btn-success btn-lg">
+                <i class="icofont-check mr-1"></i>
+                Registrar ponto
+            </a>
+        </div>
+    </div>
 
 </main>
