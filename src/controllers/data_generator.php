b2c37da839deef57df8611e399780759e03a2432
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-02 13:05:06 -0300

    Geração de massa de dados #2

---
+++ src/controllers/data_generator.php
@@ -0,0 +1,67 @@
+<?php
+
+loadModel('WorkingHours');
+loadModel('User');
+
+Database::executeSQL('DELETE FROM working_hours');
+Database::executeSQL('DELETE FROM users WHERE id > 5');
+
+function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate)
+{
+    $regularDayTemplate = [
+        'time1' => '08:00:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '17:00:00',
+        'worked_time' => DAILY_TIME
+    ];
+
+    $extraHourDayTemplate = [
+        'time1' => '08:00:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '18:00:00',
+        'worked_time' => DAILY_TIME + 3600
+    ];
+
+    $lazyDayTemplate = [
+        'time1' => '08:30:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '18:00:00',
+        'worked_time' => DAILY_TIME - 1800
+    ];
+
+    $value = rand(0, 100);
+    if ($value <= $regularRate) {
+        return $regularDayTemplate;
+    } elseif ($value <= $regularRate + $extraRate) {
+        return $extraHourDayTemplate;
+    } else {
+        return $lazyDayTemplate;
+    }
+}
+
+function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate)
+{
+    $currentDate = $initialDate;
+    $today = new DateTime();
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+
+    while (isBefore($currentDate, $today)) {
+        if (!isWeekend($currentDate)) {
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
+}
+// $lastMonth = strtotime('firts day of last month');
+populateWorkingHours(1, '2020-12-1', 70, 20, 10);
+populateWorkingHours(3, '2020-12-1', 20, 75, 5);
+populateWorkingHours(4, '2020-12-1', 20, 10, 70);
+
+echo 'Tudo OK!';
