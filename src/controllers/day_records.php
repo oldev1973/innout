3f6f8b037bb45f2ac66d1bd243c81ac1c12926ae
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-03 15:14:24 -0300

    Exibir horas calculadas

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -7,10 +7,10 @@ loadModel('WorkingHours');
 $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date);
 
-$user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+// $user = $_SESSION['user'];
+// $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 loadTemplateView('day_records', [
     'today' => $today,
-    'records' => $records
+    // 'records' => $records
 ]);
