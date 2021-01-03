4fe1a2a507627454db87e96a9d98bfb9a14a265e
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-03 13:08:26 -0300

    Registro de ponto forçado

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -9,6 +9,9 @@ $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 try {
     $currentTime = strftime('%H:%M:%S', time());
+    if ($_POST['forcedTime']) {
+        $currentTime = $_POST['forcedTime'];
+    }
     $records->innout($currentTime);
     addSuccesMsg('Registro efetuado com sucesso!');
 } catch (AppException $e) {
--- src/views/day_records.php
+++ src/views/day_records.php
@@ -15,12 +15,12 @@
         </div>
         <div class="card-body">
             <div class="d-flex justify-content-around m-5">
-                <span class="record">Entrada 1: <?= $records->time1 ?? '---' ?></span>
-                <span class="record">Saída 1: <?= $records->time2 ?? '---' ?></span>
+                <span class="record">Entrada 1: <?= @$records->time1 ?? '---' ?></span>
+                <span class="record">Saída 1: <?= @$records->time2 ?? '---' ?></span>
             </div>
             <div class="d-flex justify-content-around m-5">
-                <span class="record">Entrada 2: <?= $records->time3 ?? '---' ?></span>
-                <span class="record">Saída 2: <?= $records->time4 ?? '---' ?></span>
+                <span class="record">Entrada 2: <?= @$records->time3 ?? '---' ?></span>
+                <span class="record">Saída 2: <?= @$records->time4 ?? '---' ?></span>
             </div>
         </div>
         <div class="card-footer d-flex justify-content-center">
@@ -30,5 +30,12 @@
             </a>
         </div>
     </div>
-
+    <form class="mt-5" action="innout.php" method="post">
+        <div class="input-group no-border">
+            <input type="text" name="forcedTime" class="form-control" placeholder="informe a hora para simular o registro">
+            <button class="btn btn-danger ml-3">
+                Simular Ponto
+            </button>
+        </div>
+    </form>
 </main>
