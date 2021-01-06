a01ce7012d0b436f8c4c895c2e69562434818268
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-06 15:31:20 -0300

    Consultas gerenciais #02

--- src/controllers/manager_report.php
+++ src/controllers/manager_report.php
@@ -3,4 +3,16 @@
 session_start();
 requireValidSession();
 
-loadTemplateView('manager_report', []);
+$activeUserCount = User::getActiveUserCount();
+$absentUsers = WorkingHours::getAbsentUsers();
+
+$yearAndMonth = (new DateTime())->format('Y-m');
+$workedSecondsInMonth = WorkingHours::getWorketTimeInMonth($yearAndMonth);
+
+$workedHoursInMonth = explode(':', getTimeStringFromSeconds($workedSecondsInMonth))[0];
+
+loadTemplateView('manager_report', [
+    'activeUserCount' => $activeUserCount,
+    'absentUsers' => $absentUsers,
+    'workedHoursInMonth' => $workedHoursInMonth,
+]);
--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,3 +1,11 @@
 <main class="content">
     <h1>Manager Report</h1>
+    <?php
+
+        echo "Usuários ativos: {$activeUserCount} <br>";
+        echo "Horas Trabalhadas: {$workedHoursInMonth} <br>";
+        print_r($absentUsers);
+
+
+    ?>
 </main>
