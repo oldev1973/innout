cf823578f0a2fc2d97ebbcd82bbbbb0a1bfc31f7
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-06 17:01:27 -0300

    CSS relatório gerencial

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -10,17 +10,17 @@
 
     <div class="summary-boxes">
         <div class="summary-box bg-primary">
-            <i class="icofont-users"></i>
+            <i class="icon icofont-users"></i>
             <p class="title">Quantidade de funcionários</p>
             <h3 class="value"><?= $activeUserCount?></h3>
         </div>
         <div class="summary-box bg-danger">
-            <i class="icofont-patient-bed"></i>
+            <i class="icon icofont-patient-bed"></i>
             <p class="title">Faltas</p>
             <h3 class="value"><?= count($absentUsers)?></h3>
         </div>
         <div class="summary-box bg-success">
-            <i class="icofont-sand-clock"></i>
+            <i class="icon icofont-sand-clock"></i>
             <p class="title">Horas trabalhadas no mês</p>
             <h3 class="value"><?= $workedHoursInMonth?></h3>
         </div>
@@ -30,7 +30,7 @@
         <div class="card mt-4">
             <div class="card-header">
                 <h4 class="card-title">Faltaram Hoje:</h4>
-                <p class="card-category">Relação de funcionários ausentes até o momento</p>
+                <p class="card-category mb-0">Relação de funcionários ausentes até o momento</p>
             </div>
             <div class="card-body">
                 <table class="table table-bordered table-striped table-hover">
