e2832bfaba62f2fd2f3cbf5e4b9219bac89ac462
Fernando Trindade <trindade-fernando@outlook.com>
2021-01-08 18:11:21 -0300

    Filtros de Segurança

--- src/models/Model.php
+++ src/models/Model.php
@@ -6,17 +6,25 @@ class Model
     protected static $columns = [];
     protected $values = [];
 
-    public function __construct($arr)
+    public function __construct($arr, $sanitize = true)
     {
-        $this->loadFromArray($arr);
+        $this->loadFromArray($arr, $sanitize);
     }
 
-    public function loadFromArray($arr)
+    public function loadFromArray($arr, $sanitize = true)
     {
         if ($arr) {
+            $conn = Database::getConnection();
             foreach ($arr as $key => $value) {
-                $this->$key =  $value;
+                $cleanValue = $value;
+                if ($sanitize && isset($cleanValue)) {
+                    $cleanValue = strip_tags(trim($cleanValue));
+                    $cleanValue = htmlentities($cleanValue, ENT_NOQUOTES);
+                    $cleanValue = mysqli_real_escape_string($conn, $cleanValue);
+                }
+                $this->$key =  $cleanValue;
             }
+            $conn->close();
         }
     }
 
